da1eca12f6abe2fd8b4b9caa67a67c660926bbad
Coupile <coupile.zhang@gmail.com>
2024-02-01 11:40:15 +0000

    Update prepare_package.sh

--- SCRIPTS/prepare_package.sh
+++ SCRIPTS/prepare_package.sh
@@ -11,22 +11,33 @@ sed -i '2i src-git theme https://github.com/r1172464137/openwrt_package.git;them
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
+## 取消bootstrap为默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+## 吃鹅
 mkdir -p files/usr/share/dae
 mkdir -p package/base-files/files/usr/bin
 curl -o files/usr/share/dae/geoip.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat
 curl -o files/usr/share/dae/geosite.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat
 
-curl -o package/base-files/files/usr/bin/fuck https://raw.githubusercontent.com/msylgj/R2S-R4S-OpenWrt/master/SCRIPTS/fuck
+## fuck
+curl -o package/base-files/files/usr/bin/fuck https://raw.githubusercontent.com/QiuSimons/OpenWrt-Add/master/fuck
 
+## 加入编译信息
 sed -i "s/'%D %V %C'/'Built by Coupile($(date +%Y.%m.%d))@%D %V'/g" package/base-files/files/etc/openwrt_release
 # sed -i "/DISTRIB_REVISION/d" package/base-files/files/etc/openwrt_release
 # sed -i "/%D/a\ Built by Coupile($(date +%Y.%m.%d))" package/base-files/files/etc/banner
+
+## 修改主机名和默认IP
 sed -i "s,hostname='ImmortalWrt',hostname='OpenWrt',g" package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
+## 替换node，加速编译
 rm -rf feeds/packages/lang/node
 git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt -b packages-23.05 feeds/packages/lang/node
 
+## 吃鹅
 rm -rf package/emortal/daed-next
 git clone -b rebase --depth 1 https://github.com/QiuSimons/luci-app-daed-next package/emortal/daed-next
 find ./package/emortal/daed-next/luci-app-daed-next/root/etc -type f -exec chmod +x {} \;
